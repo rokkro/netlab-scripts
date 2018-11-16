4338cae222b8ed6df9ca2a43d00d882bee1f5515
rukkro <rukkro@users.noreply.github.com>
2018-11-16 15:33:30 -0600

    Domain specific routing

--- resetPCnetSettings.ps1
+++ resetPCnetSettings.ps1
@@ -5,7 +5,7 @@
 # Get names of all adapters on PC
 $all_adapters = Get-NetAdapter | Select-Object -ExpandProperty "Name"
 $domain_num
-
+$number_of_domains = 6
 foreach($adapter_name in $all_adapters){
 
 	# Enable the adapter if it's disabled
@@ -50,6 +50,9 @@ foreach($adapter_name in $all_adapters){
 		# Get substring of IP address to get domain_num. Second arg of .substring() is the length of substring 
 		$domain_num = $ipv4_address.substring($dot_before_last_dot,($last_dot - $dot_before_last_dot))
 		echo "Domain number is " $domain_num
+		
+		# Prevent domain adapter from DNS registration
+		$adapter | set-dnsclient -RegisterThisConnectionsAddress $false
 	}
 }
 
@@ -68,19 +71,21 @@ $interface_index = $adapter | Select-Object -ExpandProperty InterfaceIndex
 
 # Add persistent route
 route -p add 0.0.0.0 mask 0.0.0.0 10.0.0.3 if $interface_index
+route -p add 10.0.0.0 mask 255.255.255.0 10.0.0.3 if $interface_index
 
 # Get adapter by name
 $adapter = Get-NetAdapter -Name "LAN Connection"
 # Getting interface index (necessary for persistent route to become active)
 $interface_index = $adapter | Select-Object -ExpandProperty InterfaceIndex
 
-# Add persistent route, using domain number found earlier
-route -p add 10.0.0.0 mask 255.255.255.0 10.0.$domain_num.1  if $interface_index
-
+# Add persistent routes. Direct domain router traffic.
+for($i=1;$i -le $number_of_domains;$i++){
+	route delete 10.0.$i.0
+	route -p add 10.0.$i.0 mask 255.255.255.0 10.0.$domain_num.1 if $interface_index
+}
 
 ############################################
 #            ENABLE FIREWALL               #
 ############################################
 echo "Enabling Firewall..."
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
-
