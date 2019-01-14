741c6cb7c1e312705c2bdff6594df38232fdc3c0
rukkro <rukkro@users.noreply.github.com>
2019-01-14 13:50:53 -0600

    Update switch_reset.tcl

--- switch_reset.tcl
+++ switch_reset.tcl
@@ -1,21 +1,22 @@
-tclsh
 puts [open "flash:reset.tcl" w+] {
-puts "Backing up startup-config"
+puts "Backing up startup-config..."
 typeahead ""
 puts [exec "copy nvram:startup-config flash:base.cfg" ]
-puts "Erasing Configuration"
+puts "Erasing configuration..."
 typeahead ""
 puts [ exec "write erase" ]
-puts "Erasing VLAN Database"
+puts "Erasing VLAN database..."
 typeahead ""
 puts [ exec "del flash:vlan.dat" ]
-puts "Copying Base Configuration To Startup"
+puts "Copying backup to startup-config..."
 typeahead ""
 puts [ exec "copy flash:base.cfg nvram:startup-config" ]
 typeahead ""
+puts "Copying startup-config to running-config..."
+typeahead ""
 puts [ exec "copy nvram:startup-config nvram:running-config" ]
 typeahead ""
-puts "Reloading device to generate new VLAN Database"
+puts "Reloading device to generate new VLAN Database..."
 typeahead ""
 puts [ exec "reload" ]
 typeahead ""
