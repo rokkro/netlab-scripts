474e22ce94207d86927afc27499c7c5ae988d4ac
rukkro <rukkro@users.noreply.github.com>
2019-01-14 13:56:44 -0600

    Switch to carriage returns

--- switch_reset.tcl
+++ switch_reset.tcl
@@ -1,23 +1,23 @@
 puts [open "flash:reset.tcl" w+] {
 puts "Backing up startup-config..."
-typeahead ""
+typeahead "\r"
 puts [exec "copy nvram:startup-config flash:base.cfg" ]
 puts "Erasing configuration..."
-typeahead ""
+typeahead "\r"
 puts [ exec "write erase" ]
 puts "Erasing VLAN database..."
-typeahead ""
+typeahead "\r"
 puts [ exec "del flash:vlan.dat" ]
 puts "Copying backup to startup-config..."
-typeahead ""
+typeahead "\r"
 puts [ exec "copy flash:base.cfg nvram:startup-config" ]
-typeahead ""
+typeahead "\r"
 puts "Copying startup-config to running-config..."
-typeahead ""
+typeahead "\r"
 puts [ exec "copy nvram:startup-config nvram:running-config" ]
-typeahead ""
+typeahead "\r"
 puts "Reloading device to generate new VLAN Database..."
-typeahead ""
+typeahead "\r"
 puts [ exec "reload" ]
-typeahead ""
+typeahead "\r"
 }
