a772a16f63cff0c9aa319a2548a4621c99c6aa36
rukkro <rukkro@users.noreply.github.com>
2019-01-14 13:22:20 -0600

    Update router_reset.tcl

--- router_reset.tcl
+++ router_reset.tcl
@@ -3,9 +3,11 @@ puts [open "flash:reset.tcl" w+] {
 puts "Backing up startup-config"
 typeahead ""
 puts [exec "copy nvram:startup-config flash:base.cfg" ]
+typeahead ""
 puts "Erasing Configuration"
 typeahead ""
 puts [ exec "write erase" ]
+typeahead ""
 puts "Copying Base Configuration To Startup"
 typeahead ""
 puts [ exec "copy flash:base.cfg nvram:startup-config" ]
