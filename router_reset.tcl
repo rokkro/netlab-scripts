d83ca32294e48e60e0532896ac0008e0266c5182
rukkro <rukkro@users.noreply.github.com>
2019-01-21 18:20:53 -0600

    Update router_reset.tcl

--- router_reset.tcl
+++ router_reset.tcl
@@ -1,5 +1,5 @@
 puts [open "flash:reset.tcl" w+] {
-puts "Backing up startup-config..."
+puts "Backing up startup-config to flash..."
 puts "(Press Enter if the script pauses here)"
 typeahead "\r"
 puts [exec "copy nvram:startup-config flash:base.cfg" ]
@@ -17,7 +17,7 @@ puts "Copying startup-config to running-config..."
 typeahead "\r"
 puts [ exec "copy nvram:startup-config nvram:running-config" ]
 typeahead "\r"
-puts "Reloading device..."
+puts "Reloading device to update settings..."
 typeahead "\r"
 puts [ exec "reload" ]
 typeahead "\r"
