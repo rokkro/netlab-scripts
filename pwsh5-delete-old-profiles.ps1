172dc7babf89465385898eb200b8cb83c1958853
rukkro <rukkro@users.noreply.github.com>
2019-01-24 15:59:08 -0600

    Create pwsh5-delete-old-profiles.ps1

---
+++ pwsh5-delete-old-profiles.ps1
@@ -0,0 +1,29 @@
+# Quick script to delete old user accounts after windows server upgrade
+# Requires powershell 5, run as admin. 
+
+# Make all errors terminating. Makes some try..catch blocks actually work.
+$ErrorActionPreference = "Stop"; 
+
+# Get info on all profiles on local machine
+$localpaths = Get-WmiObject  -Class Win32_UserProfile | Select-Object  -Property LocalPath
+
+foreach($path in $localpaths){
+	# Converts it to an array
+	$path = $path | Out-String -Stream 
+
+	# path is now an object array, the LocalPath is in the third position
+	$path = $path[3] 
+
+	"Path is " + $path
+
+	# If it's from the old domain, and is not the local machine netlab account, delete it!
+	if(!$path.endsWith('TM') -and $path.startsWith('C:\Users\') -and !$path.endswith('netlab') -and !$path.endswith('Public') -and !$path.endswith('Default')){
+		try{
+			"Deleting " + $path + "..."
+			Get-WmiObject -Class Win32_UserProfile | where {($_.LocalPath -eq $path)} | Remove-WmiObject  
+		}catch{
+			"Error deleting the entire profile!"
+			"*** Manually delete the profile directory at " + $path + " ***"
+		}
+	}
+}
