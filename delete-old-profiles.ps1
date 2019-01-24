4b307feccc331a8b7580941d2afa0408acb88615
rukkro <rukkro@users.noreply.github.com>
2019-01-24 14:07:57 -0600

    Update for powershell 6
    Remove-Item has a random error. Probably a bug in powershell.

--- delete-old-profiles.ps1
+++ delete-old-profiles.ps1
@@ -5,7 +5,7 @@
 $ErrorActionPreference = "Stop"; 
 
 # Get info on all profiles on local machine
-$localpaths = Get-WMIObject -Class Win32_UserProfile | Select-Object  -Property LocalPath
+$localpaths = Get-CimInstance  -Class Win32_UserProfile | Select-Object  -Property LocalPath
 
 foreach($path in $localpaths){
 	# Converts it to an array
@@ -13,13 +13,14 @@ foreach($path in $localpaths){
 	
 	# path is now an object array, the LocalPath is in the third position
 	$path = $path[3] 
-
+	"Path is " + $path
 	# If it's from the old domain, and is not the local machine netlab account, delete it!
 	if(!$path.endsWith('TM') -and $path.startsWith('C:\Users\') -and !$path.endswith('netlab') -and !$path.endswith('Public') -and !$path.endswith('Default')){
 		try{
 			"Deleting " + $path + "..."
-			Get-WMIObject -Class Win32_UserProfile | where {($_.LocalPath -eq $path)} | Remove-WMIObject  
+			Get-CimInstance -Class Win32_UserProfile | where {($_.LocalPath -eq $path)} | Remove-CimInstance  
 		}catch{
+			"Threw an error. Deleting profile path at " + $path
 			Remove-Item -path $path -recurse -force
 		}
 	}
